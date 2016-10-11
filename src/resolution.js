fdebf78f328b2d60290e4c3d7f1e815312e1858a
feizheng <1290657123@qq.com>
2016-10-11 23:34:59 +0800

    update to 50px base

--- src/resolution.js
+++ src/resolution.js
@@ -3,25 +3,22 @@
   'use strict';
 
   var docEle = doc.documentElement,
-    metaEle = doc.createElement('meta'),
     dpr = Math.min(win.devicePixelRatio, 3),
     scale = 1 / dpr,
     resizeEvent = 'orientationchange' in win ? 'orientationchange' : 'resize';
 
   dpr = win.top === win.self ? dpr : 1;
   docEle.dataset.dpr = dpr;
-  metaEle.name = 'viewport';
-  metaEle.content = 'initial-scale=' + scale + ',maximum-scale=' + scale;
-  docEle.firstElementChild.appendChild(metaEle);
 
   //initial:
   (function recalCulate() {
     var width = docEle.clientWidth;
-    if (width / dpr > 640) {
-      width = 640 * dpr;
+    if (width / dpr > 750) {
+      width = 750 * dpr;
     }
-    docEle.style.fontSize = 20 * (width / 750) + 'px';
-
+    docEle.dataset.width = width;
+    docEle.dataset.percent = 100 * (width / 750);
+    docEle.style.fontSize = 100 * (width / 750) + 'px';
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvent, recalCulate, false);
   }());
